efd92e1b439a05d2ec5eb077fc78091cb2da555b
isna <isnaainurrohmah2005@gmail.com>
2026-08-28 15:08:41 +0700

    fix paths and deployment

--- app/Config/Paths.php
+++ app/Config/Paths.php
@@ -2,77 +2,40 @@
 
 namespace Config;
 
-/**
- * Paths
- *
- * Holds the paths that are used by the system to
- * locate the main directories, app, system, etc.
- *
- * Modifying these allows you to restructure your application,
- * share a system folder between multiple applications, and more.
- *
- * All paths are relative to the project's root folder.
- *
- * NOTE: This class is required prior to Autoloader instantiation,
- *       and does not extend BaseConfig.
- */
 class Paths
 {
     /**
-     * ---------------------------------------------------------------
      * SYSTEM FOLDER NAME
-     * ---------------------------------------------------------------
      *
-     * This must contain the name of your "system" folder. Include
-     * the path if the folder is not in the same directory as this file.
+     * Path relatif dari lokasi file Paths.php ke folder system.
      */
-    public string $systemDirectory = __DIR__ . '/../../vendor/codeigniter4/framework/system';
+    public string $systemDirectory = __DIR__ . '/../../system';
 
     /**
-     * ---------------------------------------------------------------
      * APPLICATION FOLDER NAME
-     * ---------------------------------------------------------------
      *
-     * If you want this front controller to use a different "app"
-     * folder than the default one you can set its name here. The folder
-     * can also be renamed or relocated anywhere on your server. If
-     * you do, use a full server path.
-     *
-     * @see http://codeigniter.com/user_guide/general/managing_apps.html
+     * Path relatif dari lokasi file Paths.php ke folder app.
      */
     public string $appDirectory = __DIR__ . '/..';
 
     /**
-     * ---------------------------------------------------------------
-     * WRITABLE DIRECTORY NAME
-     * ---------------------------------------------------------------
+     * WRITABLE FOLDER NAME
      *
-     * This variable must contain the name of your "writable" directory.
-     * The writable directory allows you to group all directories that
-     * need write permission to a single place that can be tucked away
-     * for maximum security, keeping it out of the app and/or
-     * system directories.
+     * Path relatif dari lokasi file Paths.php ke folder writable.
      */
     public string $writableDirectory = __DIR__ . '/../../writable';
 
     /**
-     * ---------------------------------------------------------------
-     * TESTS DIRECTORY NAME
-     * ---------------------------------------------------------------
+     * TESTS FOLDER NAME
      *
-     * This variable must contain the name of your "tests" directory.
+     * Path relatif dari lokasi file Paths.php ke folder tests.
      */
     public string $testsDirectory = __DIR__ . '/../../tests';
 
     /**
-     * ---------------------------------------------------------------
-     * VIEW DIRECTORY NAME
-     * ---------------------------------------------------------------
+     * VIEW FOLDER NAME
      *
-     * This variable must contain the name of the directory that
-     * contains the view files used by your application. By
-     * default this is in `app/Views`. This value
-     * is used when no value is provided to `Services::renderer()`.
+     * Path relatif dari lokasi file Paths.php ke folder views.
      */
     public string $viewDirectory = __DIR__ . '/../Views';
 }
